7242a419c2a8511f85eb7c66f0003231842df77f
rafaelturse <rafaelturse@hotmail.com>
2021-10-25 20:52:30 -0700

    hello .net

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using ProAgile.WebAPI.Model;
+
+namespace ProAgile.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Event>> Get()
+        {
+            return new Event[] {
+                new Event() {
+                    id = 1,
+                    place = "Toronto Island",
+                    date = DateTime.Now.AddDays(2).ToString("dd/MM/yyy"),
+                    theme = "Angular and .NET Core",
+                    peopleAmount = 1,
+                    lot = "1o lot"
+                },
+                new Event() {
+                    id = 2,
+                    place = "Toronto Island",
+                    date = DateTime.Now.AddDays(2).ToString("dd/MM/yyy"),
+                    theme = "React and Java",
+                    peopleAmount = 1,
+                    lot = "1o lot"
+                }
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Event> Get(int id)
+        {
+            return new Event[] {
+                new Event() {
+                    id = 1,
+                    place = "Toronto Island",
+                    date = DateTime.Now.AddDays(2).ToString("dd/MM/yyy"),
+                    theme = "Angular and .NET Core",
+                    peopleAmount = 1,
+                    lot = "1o lot"
+                },
+                new Event() {
+                    id = 2,
+                    place = "Toronto Island",
+                    date = DateTime.Now.AddDays(2).ToString("dd/MM/yyy"),
+                    theme = "React and Java",
+                    peopleAmount = 1,
+                    lot = "1o lot"
+                }
+            }.FirstOrDefault(x => x.id == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Model/Event.cs
@@ -0,0 +1,17 @@
+namespace ProAgile.WebAPI.Model
+{
+    public class Event
+    {
+      public int id { get; set; }   
+
+      public string place { get; set; }   
+
+      public string date { get; set; }   
+
+      public string theme { get; set; }   
+
+      public int peopleAmount { get; set; }   
+
+      public string lot { get; set; }   
+    }
+}
